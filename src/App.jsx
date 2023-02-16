468c855154203819320e8cb111950b8f81d52940
thiengoten <thiengoten6@gmail.com>
2023-02-16 20:05:11 +0700

    Setup React-router-dom and delete some svg file

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+    createBrowserRouter,
+    Routes,
+    Route,
+    Link,
+    NavLink,
+    createRoutesFromElements,
+    RouterProvider,
+} from 'react-router-dom'
+import { Navbar } from './Components/NavBar/Navbar'
+import { CreatePost } from './Pages/CreatePost/CreatePost'
+import { Home } from './Pages/Home/Home'
+import { UserInfo } from './Pages/UserInfo/UserInfo'
+import { Question } from './Pages/Question/Question'
+import { Login } from './Pages/Login/Login'
+import { Logout } from './Pages/Logout/Logout'
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Navbar />}>
+            <Route path='/' element={<Home />} />
+            <Route path='create-post' element={<CreatePost />} />
+            <Route path='question' element={<Question />} />
+            <Route path='user-info' element={<UserInfo />} />
+            <Route path='login' element={<Login />} />
+            <Route path='logout' element={<Logout />} />
+        </Route>
+    )
+)
 
 function App() {
-    const [count, setCount] = useState(0)
-
-    return (
-        <div className='App'>
-            <div>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src='/vite.svg' className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://reactjs.org' target='_blank'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>Hello there</p>
-        </div>
-    )
+    return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+
+export const Navbar = () => {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink to='/'>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/create-post'>Create Post</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/question'>Question</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/user-info'>UserInfo</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/login'>Login</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/logout'>Logout</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <main>
+                    <Outlet />
+                </main>
+            </header>
+        </div>
+    )
+}
