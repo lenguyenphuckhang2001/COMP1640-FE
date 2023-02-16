920caf07e211807e321f689397234fd1726d5561
thiengoten <thiengoten6@gmail.com>
2023-02-17 00:29:33 +0700

    Adding some prettier settup and adding some package and test it

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
-
+import React from 'react';
+import './Home.scss';
 export const Home = () => {
-    return <div>Home</div>
-}
+  return (
+    <div>
+      <h1>Home</h1>
+      <div className='scssClass'>
+        <p>Hello</p>
+        <h2>Hi</h2>
+      </div>
+    </div>
+  );
+};
