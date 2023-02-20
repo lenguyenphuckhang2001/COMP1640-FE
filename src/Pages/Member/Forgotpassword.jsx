e1a67c06f33be5739b5efaecd222883abfb7cfd2
huydang <danghuy6584@gmail.com>
2023-02-20 21:08:05 +0700

    create forgotpass

--- src/Pages/Member/Forgotpassword.jsx
+++ src/Pages/Member/Forgotpassword.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
-import './Forgotpassword.scss'
+import './Forgotpassword.scss';
 import { Link } from 'react-router-dom';
 export const Forgotpassword = () => {
-    return(
-      <div className='Login-page'>
+  return (
+    <div className='Login-page'>
       <div className='Auth-form-container'>
         <form className='Auth-form'>
           <div className='Auth-form-content'>
-            <h3 className='.Auth-form-title-forgotpass'>Forgot Paswword</h3>
+            <h3 className='Auth-form-title-forgotpass'>Forgot Paswword</h3>
+            <p className='fg-p'>Dont wory. We can help.</p>
             <div className='form-group mt-3'>
-              <input type='email' className='form-control mt-1' placeholder='Please fill in your email address' />
+              <input
+                type='email'
+                className='form-control mt-1'
+                placeholder='Please fill in your email address'
+              />
             </div>
-            <p className='forgot-password text-right mt-2'>
-            </p>
-            <div className='d-grid gap-2 mt-3'>
+            <p className='forgot-password text-right mt-2'></p>
+            <div className='fg-bt'>
+              <span>
+                <p>Rememberd your password?</p>
+                <Link to='/login' className='fg-link'>
+                  Back to login
+                </Link>
+              </span>
               <button type='submit' className='btn btn-primary'>
                 Continue
               </button>
             </div>
           </div>
         </form>
-        <p className='register'>
-            Don’t have an account?
-        </p>
-        <Link to="/login">Login</Link>
       </div>
     </div>
-    )
-}
+  );
+};
