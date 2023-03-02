061a642424fa4503b797ee0b09bcddc9e5d1b51d
Trương Văn Quang <truongvanquang@Quang-Trng.local>
2023-03-02 19:41:24 +0700

    fix tag input, create downloadfile, add uploadfile in question

--- src/Pages/CreatePost/CreatePost.jsx
+++ src/Pages/CreatePost/CreatePost.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import axios from 'axios';
 import { Col } from 'react-bootstrap';
 import { BiUnderline } from 'react-icons/Bi';
 import { BiItalic } from 'react-icons/Bi';
@@ -8,22 +9,20 @@ import { BiBold } from 'react-icons/Bi';
 import './CreatePost.scss';
 
 export const CreatePost = (props) => {
-
-  
   // Set tag
   const [tags, setTags] = useState(props.tags || []);
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
   const handleInputKeyDown = (event) => {
-    if (event.key === "Enter" && inputValue.trim() !== "") {
+    if (event.key === 'Enter' && inputValue.trim() !== '') {
       const newTags = [...tags];
       newTags.push(inputValue.trim());
       setTags(newTags);
-      setInputValue("");
+      setInputValue('');
     }
   };
 
@@ -34,30 +33,50 @@ export const CreatePost = (props) => {
   };
 
   //Set FileUpload
+  const [selectedFile, setSelectedFile] = useState(null);
+
   const handleFileInputChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
 
-  const handleFileUpload = () => {
+  const handleFileUpload = async () => {
     const formData = new FormData();
     formData.append('file', selectedFile);
 
-    // You can send the form data to a server-side endpoint here
-    console.log(formData);
+    // Send the form data to a server-side endpoint to handle the file upload
+    const response = await fetch('/api/upload', {
+      method: 'POST',
+      body: formData,
+    });
+
+    console.log(await response.json());
   };
 
+  const handleFileDownload = async () => {
+    // Send a request to a server-side endpoint to download the file as a zip archive
+    const response = await fetch('/api/download', {
+      method: 'GET',
+      responseType: 'blob',
+    });
+
+    // Create a URL for the downloaded file and open it in a new window
+    const url = window.URL.createObjectURL(await response.blob());
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'download.zip');
+    document.body.appendChild(link);
+    link.click();
+  };
 
   //Set checkbox
 
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedCheck, setSelectedCheck] = useState(null);
   const [isChecked, setIsChecked] = useState(false);
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
   };
 
-  
   return (
-
     <Col md={{ span: 6, offset: 3 }}>
       {/* createpost content */}
       <div className='createpost'>
@@ -80,16 +99,56 @@ export const CreatePost = (props) => {
             <input
               className='ip_question'
               type='text'
-              id='fname'
-              name='fname'
-              placeholder='&#xf007;Write description'
+              id='frame'
+              name='frame'
+              placeholder='&#xf007; Write description'
             />
+
+            {/* Button UploadFile */}
+            <div className='button_item'>
+            <button className='upload_button' onClick={handleFileUpload}>
+                  Upload File
+                </button>
+            </div>
+
+            {/* Add Tag */}
+            <div className='add-tag-input'>
+            <div>
+              <input
+                type='text'
+                placeholder='Add tag...'
+                value={inputValue}
+                onKeyDown={handleInputKeyDown}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <ul>
+                {tags.map((tag, index) => (
+                  <li key={index}>
+                    {tag}
+                    <button onClick={() => handleRemoveTag(index)}>x</button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
             {/* function upload */}
 
-            <div className='UploadFile'>
-              <p>File Upload</p>
-              <input type='file' onChange={handleFileInputChange} />
-              <button disabled={!selectedFile} onClick={handleFileUpload}>Upload File</button>
+            <div className='file-upload-and-download'>
+              <h1 className='title'>File Upload and Download</h1>
+              <div className='file-input'>
+                <label htmlFor='file'>Choose a file:</label>
+                <input type='file' id='file' onChange={handleFileInputChange} />
+              </div>
+              <div className='button-group'>
+                <button className='upload-button' onClick={handleFileUpload}>
+                  Upload File
+                </button>
+                <button className='download-button' onClick={handleFileDownload}>
+                  Download File
+                </button>
+              </div>
             </div>
             <br />
             {/* function checkbox */}
@@ -100,30 +159,6 @@ export const CreatePost = (props) => {
             {/* button */}
             <button className='bt_post'>Post</button>
           </form>
-          {/* Tag */}
-          <div className='add-tag-input'>
-          <div>
-                <input
-                  type='text'
-                  placeholder='Add tag...'
-                  value={inputValue}
-                  onKeyDown={handleInputKeyDown}
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div >
-                <ul>
-                  {tags.map((tag, index) => (
-                    <li key={index}>
-                      {tag}
-                      <button onClick={() => handleRemoveTag(index)}>x</button>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              
-            </div>
-            
         </div>
       </div>
     </Col>
