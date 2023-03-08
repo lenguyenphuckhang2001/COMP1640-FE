614071053e9801506455a2f0a9b2683e29dc8a01
Trương Văn Quang <truongvanquang@Quang-Trng.local>
2023-03-09 05:48:06 +0700

    fix Navbar

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,63 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsFillGearFill, BsFillQuestionCircleFill, BsPersonCircle } from 'react-icons/Bs';
 import { AiFillHome, AiFillPlusCircle } from 'react-icons/Ai';
 import { IoMdNotifications } from 'react-icons/Io';
-import { NavLink,} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
 import './Navbar.scss';
 
 export const Navbar = () => {
-  return (
-      <header>
-        <nav>
-          <ul className='Navbar_item'>
-            <p className='icon_logo'>
-              <NavLink className='item' to='/'>
-                <BsFillGearFill />
-              </NavLink>
-            </p>
-            <p className='text_logo'>
-              <NavLink className='item' to='/'>
-                BlueFoum
-              </NavLink>
-            </p>
-            <p className='icon_home'>
-              <NavLink className='item' to='/'>
-                <AiFillHome />
-              </NavLink>
-            </p>
-            <p className='search_item'>
-              <input
-                className='item_search'
-                type='tel'
-                id='phone '
-                name='phone'
-                placeholder='&#xf002;   Search for anything'
-              />
-            </p>
+  const [showDropdown, setShowDropdown] = useState(false);
 
-            <p className='createpost_icon'>
-              <NavLink className='item' to='/create-post'>
-                <AiFillPlusCircle />
-              </NavLink>
-            </p>
-            <p className='notification_icon'>
-              <p className='item'>
-                <IoMdNotifications />
-              </p>
-            </p>
-            <p className='question_icon'>
-              <NavLink className='item' to='/question'>
-                <BsFillQuestionCircleFill />
-              </NavLink>
-            </p>
-            <p className='userinfo_icon'>
-              <NavLink className='item' to='Account/user-info'>
-                <BsPersonCircle />
-              </NavLink>
-            </p>
-          </ul>
-        </nav>
-      </header>
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+  return (
+    <header>
+      <nav>
+        <Row>
+          <div>
+            <div className='row navbar'>
+              <div className='col-12 col-md-2'>
+                <div className='logo'>
+                 
+                  <a className='icon' href="#"><BsFillGearFill /></a>
+                    <a className='text' href="#">BlueFoum</a>
+                 
+                </div>
+              </div>
+
+              <div className='col-12 col-md-1'>
+                <div className='home'>
+                  <NavLink to='/'>
+                    <AiFillHome />
+                  </NavLink>
+                </div>
+              </div>
+
+              <div className='col-12 col-md-6'>
+                <div className='search'>
+                  <input placeholder='&#xf002;  Search for anything.....'></input>
+                </div>
+              </div>
+
+              <div className='col-12 col-md-3'>
+                <div className='list_item'>
+                  <div className='post'>
+                    <NavLink to='/create-post'>
+                      <AiFillPlusCircle />
+                    </NavLink>
+                  </div>
+                  <div>
+                    <div className='notification'>
+                      <a href="#"><IoMdNotifications /></a>
+                    </div>
+                  </div>
+                  <div className='question'>
+                    <NavLink to='/question'>
+                      <BsFillQuestionCircleFill />
+                    </NavLink>
+                  </div>
+                  <div onClick={toggleDropdown} className='info'>
+                 <a href="#"> <BsPersonCircle /></a>
+                  </div>
+                </div>
+              </div>
+            {showDropdown && (
+              <div className="navbar__dropdown">
+                <NavLink className='user_item' to='Account/user-info'>User-Info</NavLink>
+                <NavLink className='logout' to='logout'>Logout</NavLink>
+              </div>
+            )}
+            </div>
+          </div>
+        </Row>
+      </nav>
+    </header>
   );
 };
 export default Navbar;
