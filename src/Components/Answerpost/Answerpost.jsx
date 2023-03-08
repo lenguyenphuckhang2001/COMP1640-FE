ff8bc8043a835992b437ede6507281a0a93f4a75
huydang <danghuy6584@gmail.com>
2023-03-08 23:08:46 +0700

    update1

--- src/Components/Answerpost/Answerpost.jsx
+++ src/Components/Answerpost/Answerpost.jsx
@@ -3,8 +3,7 @@ import './Answerpost.scss';
 import { RxFontBold, RxFontItalic } from 'react-icons/Rx';
 import { BsLink, BsCardImage } from 'react-icons/Bs';
 import JoditEditor from "jodit-react";
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 
 export const Answerpost = () => {
   return (
@@ -32,24 +31,7 @@ export const Answerpost = () => {
         <textarea placeholder="Enter your summary of poem"/>
         <button>Submit</button>
       </form> */}
-      <CKEditor
-                    editor={ ClassicEditor }
-                    data=""
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
+
     </div>
   );
 };
