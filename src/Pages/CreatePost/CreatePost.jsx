625c098a4dd2d728cdbfef1d395df61e850977c0
quangtruong <84627710+quangtruong123abc@users.noreply.github.com>
2023-03-08 23:07:28 +0700

    Feature/quang/create post page function (#13)
    * add choose, upload and checkbox
    
    * create tag input tag
    
    * fix tag input, create downloadfile, add uploadfile in question
    
    * function checkbox only when you tick box will you be allowed to Post
    
    * change function tag
    
    * fix tag and function upload
    
    * add button upload
    
    * onclick change style
    
    * fix change style
    
    ---------
    
    Co-authored-by: Trương Văn Quang <truongvanquang@Quang-Trng.local>

--- src/Pages/CreatePost/CreatePost.jsx
+++ src/Pages/CreatePost/CreatePost.jsx
@@ -1,48 +1,214 @@
 import React from 'react';
+import JSZip from 'jszip';
+import { useState } from 'react';
 import { Col } from 'react-bootstrap';
 import { BiUnderline } from 'react-icons/Bi';
 import { BiItalic } from 'react-icons/Bi';
 import { BiBold } from 'react-icons/Bi';
-
 import './CreatePost.scss';
 
-export const CreatePost = () => {
+export const CreatePost = (props) => {
+  //set keybroard
+  const [bold, setBold] = useState(false);
+  const [italic, setItalic] = useState(false);
+  const [underline, setUnderline] = useState(false);
+
+  const handleBoldClick = (e) => {
+    e.preventDefault()
+    setBold(!bold);
+  };
+
+  const handleItalicClick = (e) => {
+    e.preventDefault()
+    setItalic(!italic);
+  };
+
+  const handleUnderlineClick = (e) => {
+    e.preventDefault()
+    setUnderline(!underline);
+  };
+
+  const textStyle = {
+    fontWeight: bold ? 'bold' : 'normal',
+    fontStyle: italic ? 'italic' : 'normal',
+    textDecoration: underline ? 'underline' : 'none',
+  };
+
+  // Set tag
+
+  const options = [
+    { label: 'Javascripts', value: 'Javascripts' },
+
+    { label: 'ReactJs', value: 'ReactJs' },
+
+    { label: 'Bootraps', value: 'Bootraps' },
+
+    { label: 'Html', value: 'Html' },
+
+    { label: 'Scss', value: 'Scss' },
+  ];
+
+  const [value, setValue] = React.useState('Language');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  //Set FileUpload
+  const [file, setFile] = useState(null);
+
+  function handleUpload() {
+    const formData = new FormData();
+    formData.append('file', file);
+    fetch('url', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log('success', result);
+      })
+      .catch((error) => {
+        console.error('Error', error);
+      });
+  }
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleUploadAndDownload = () => {
+    if (file) {
+      const formData = new FormData();
+      formData.append('file', file);
+      fetch('/upload', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => response.blob())
+        .then((blob) => {
+          const zip = new JSZip();
+          zip.file(file.name, blob);
+          zip.generateAsync({ type: 'blob' }).then((content) => {
+            const url = window.URL.createObjectURL(content);
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', `${file.name}.zip`);
+            document.body.appendChild(link);
+            link.click();
+          });
+        });
+    } else {
+      alert('Please choose a file first');
+    }
+  };
+
+  //Set checkbox
+
+  const [isChecked, setIsChecked] = useState(false);
+  const [postText, setPostText] = useState('');
+
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked);
+  };
+
+  const handlePostTextChange = (event) => {
+    setPostText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (isChecked) {
+      // Send the post to a server-side endpoint
+      console.log(`Submitting post: ${postText}`);
+    } else {
+      console.log('Checkbox must be checked to submit post');
+    }
+  };
+
   return (
     <Col md={{ span: 6, offset: 3 }}>
+      {/* createpost content */}
       <div className='createpost'>
         <div className='center'>
           <form>
             <p className='title'> Create a post</p>
             <input className='ip_title' type='text' id='fname' name='fname' placeholder='Title' />
-            <p className='post'>Post</p>
-            <div className='text-icon'>
-              <span className='icon'> 
+            <p className='question'>Question</p>
+            <div className='icon_item'>
+              <button className='icon' onClick = {(e)=>{handleBoldClick(e)}}>
                 <BiBold />
-              </span>
-              <span className='icon'>
+              </button>
+              <button className='icon' onClick = {(e)=>{handleItalicClick(e)}}>
                 <BiItalic />
-              </span>
-              <span className='icon'>
+              </button>
+              <button className='icon' onClick = {(e)=>{handleUnderlineClick(e)}}>
                 <BiUnderline />
-              </span>
+              </button>
             </div>
-            <input
-              className='ip_post'
-              type='text'
-              id='fname'
-              name='fname'
-              placeholder='&#xf007;Write description'
-            />
-            <p className='img'>IMG</p>
-            <input
-              className='ip_img'
-              type='text'
-              id='fname'
-              name='fname'
-              placeholder='Upload or drop file here'
-            />
+            <input className='ip_question' style={textStyle} placeholder=' Write description....' />
+
+            {/* select tag */}
+            <div className='add-tag'>
+              <label>
+                <div className='text'>Do you want to choose language</div>
+
+                <select value={value} onChange={handleChange}>
+                  {options.map((option) => (
+                    <option value={option.value}>{option.label}</option>
+                  ))}
+                </select>
+              </label>
+
+              <div className='tag_test'>
+                <p> {value}</p>
+              </div>
+            </div>
+
+            {/* function upload and download */}
+            <div className='file-upload-and-download'>
+              <label htmlFor='file-input' className='file-upload-and-download__label'>
+                <input
+                  type='file'
+                  id='file-input'
+                  className='file-upload-and-download__input'
+                  onChange={handleFileChange}
+                />
+                <span className='file-upload-and-download__text'>
+                  {file ? file.name : 'Choose file'}
+                </span>
+              </label>
+              <div className='file-up-and-down'>
+                <form className='upload__form' onSubmit={handleUpload}>
+                  <button>Upload</button>
+                </form>
+                <button className='download__button' onClick={handleUploadAndDownload}>
+                  Download as ZIP
+                </button>
+              </div>
+            </div>
+
             <br />
-            <button className='bt_post'>Post</button>
+            {/* function checkbox */}
+            <div className='post-form'>
+              <form onSubmit={handleSubmit}>
+                <div className='form-group'>
+                  <input
+                    type='checkbox'
+                    id='checkbox'
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
+                  />
+                  <label htmlFor='checkbox'>I agree to the terms and conditions</label>
+                </div>
+                <div className='button_post'>
+                  <button type='submit' disabled={!isChecked}>
+                    Post
+                  </button>
+                </div>
+              </form>
+            </div>
           </form>
         </div>
       </div>
