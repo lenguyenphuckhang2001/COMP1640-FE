4ef5d0353ce54a608f49684893c50a17070cc611
Trương Văn Quang <truongvanquang@Quang-Trng.local>
2023-03-09 22:00:45 +0700

    fix footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,40 +8,142 @@ import { BsFacebook } from 'react-icons/Bs';
 import { AiFillInstagram } from 'react-icons/Ai';
 import { GrYoutube } from 'react-icons/Gr';
 import { SiTiktok } from 'react-icons/Si';
+import Row from 'react-bootstrap/Row';
 
 import './Footer.scss';
 
 export const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="container">
+    <footer className='footer'>
+      <div className='container'>
         <div className='logo'>
-          <BsFillGearFill/>
+          <BsFillGearFill />
           <p className='text'>BlueFoum</p>
         </div>
-        <ul className="footer_list ">
-          <li className="footer_item"><p>ABOUT US</p></li>
-          <li className="footer_item"><p>SERVICE</p></li>
-          <li className="footer_item"><p>ADRESS</p></li>
-          <li className="footer_item"><p>SOCIAL MEDIA</p></li>
-          <li className="footer_item"><a href="#">About</a></li>
-          <li className="footer_item"><a href="#">Writing</a></li>
-          <li className="footer_item"><a href="#"><p><AiOutlineHome/> </p> Ngu Hanh Son, Da Nang City</a></li>
-          <li className="footer_item"><a href="#"><p><BsFacebook/> </p> Facebook</a></li>
-          <li className="footer_item"><a href="#">Project</a></li>
-          <li className="footer_item"><a href="#">Coding</a></li>
-          <li className="footer_item"><a href="#"><p><MdOutlineEmail/> </p> Exmample@gmail.com</a></li>
-          <li className="footer_item"><a href="#"><p><AiFillInstagram/> </p> Instagram</a></li>
-          <li className="footer_item"><a href="#">Blog</a></li>
-          <li className="footer_item"><a href="#">Testing</a></li>
-          <li className="footer_item"><a href="#"><p><BiPhoneCall/> </p> +84 979 822 999</a></li>
-          <li className="footer_item"><a href="#"><p><GrYoutube/> </p> Youtube</a></li>
-          <li className="footer_item"><a href="#">Awards</a></li>
-          <li className="footer_item"><a href="#">Teaching</a></li>
-          <li className="footer_item"><a href="#"><p><ImSkype/> </p> skype:you_online</a></li>
-          <li className="footer_item"><a href="#"><p><SiTiktok/> </p> Titok</a></li>
-        </ul>
-        <p className="footer_copy">@2023 Copyright: BlueFoum</p>
+        <div className='container'>
+          <div className='row footer'>
+            <div className='col-12 col-md-3'>
+              <div className='about'>
+                <ul className='about_item'>
+                  <p>ABOUT US</p>
+                  <li className='footer_item'>
+                    <a href='#'>About</a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>Project</a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>Blog</a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>Awards</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className='col-12 col-md-3'>
+              <div className='service'>
+                <ul className='service_item'>
+                  <p>SERVICE</p>
+                  <li className='footer_item'>
+                    <a href='#'>Writing</a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>Coding</a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>Testing</a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>Teaching</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className='col-12 col-md-3'>
+              <div className='adress'>
+                <ul className='adress_item'>
+                  <p>ADRESS</p>
+                  <li className='footer_item'>
+                    <a href='#'>
+                      <p>
+                        <AiOutlineHome />
+                      </p>
+                      Da Nang City
+                    </a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>
+                      <p>
+                        <MdOutlineEmail />
+                      </p>
+                      Exmample@gmail.com
+                    </a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>
+                      <p>
+                        <BiPhoneCall />
+                      </p>
+                      +84 979 822 999
+                    </a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>
+                      <p>
+                        <ImSkype />
+                      </p>
+                      skype:you_online
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className='col-12 col-md-3'>
+              <div className='social'>
+                <ul className='social_item'>
+                  <p>SOCIAL MEDIA</p>
+                  <li className='footer_item'>
+                    <a href='#'>
+                      <p>
+                        <BsFacebook />
+                      </p>
+                      Facebook
+                    </a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>
+                      <p>
+                        <AiFillInstagram />
+                      </p>
+                      Instagram
+                    </a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>
+                      <p>
+                        <GrYoutube />
+                      </p>
+                      Youtube
+                    </a>
+                  </li>
+                  <li className='footer_item'>
+                    <a href='#'>
+                      <p>
+                        <SiTiktok />
+                      </p>
+                      Titok
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p className='footer_copy'>@2023 Copyright: BlueFoum</p>
       </div>
     </footer>
   );
