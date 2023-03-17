3b82e053e6da7388855fdab9f056d3b496a4377c
Trương Văn Quang <truongvanquang@Quang-Trng.local>
2023-03-17 18:31:28 +0700

    add tag anf crud department

--- src/Pages/QaManagement/Department/Department.jsx
+++ src/Pages/QaManagement/Department/Department.jsx
@@ -1,15 +1,125 @@
-import React from 'react'
+import React, { useState } from 'react';
+import TagsInput from 'react-tagsinput';
+import 'react-tagsinput/react-tagsinput.css';
+import { Col } from 'react-bootstrap';
+import './Department.scss';
+
 export const Department = () => {
+  const [tags, setTags] = useState([]);
+
+  function handleChange(tags) {
+    setTags(tags);
+  }
+  const [items, setItems] = useState([]);
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [date, setDate] = useState('');
+
+  const handleIdChange = (event) => {
+    setId(event.target.value);
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setDate(event.target.value);
+  };
+
+  const addItem = () => {
+    // Check if all fields are filled in
+    if (!id || !name || !date) {
+      alert('Please fill in all fields');
+      return;
+    }
+    if (!id) {
+      alert('Require ID');
+      return;
+    }
+    if (!name) {
+      alert('Require Name');
+      return;
+    }
+    if (!date) {
+      alert('Require Date');
+      return;
+    }
+
+    const newItem = {
+      id: id,
+      name: name,
+      date: date,
+    };
+    setItems([...items, newItem]);
+    setId('');
+    setName('');
+    setDate('');
+  };
+
+  const deleteItem = (index) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  };
+
   return (
-    <div>
-        <div className="row">
-            <div className="col-md-5">
-                <h2>Create Department</h2>
-            </div>
-            <div className="col-md-5">
-                <h2>Q&A Coordinator</h2>
-            </div>
+    <div className='department'>
+      <div className='row '>
+      <div className='col-md-6 '>
+        <div className='crud-department'>
+        <h2>Create Department</h2>
+        <div className='item'>
+          <div className='id'>
+            <label>ID:</label>
+            <input type='id' value={id} onChange={handleIdChange} />
+          </div>
+          <div className='name'>
+            <label>Name:</label>
+            <input type='name' value={name} onChange={handleNameChange} />
+          </div>
         </div>
+        <div className='date'>
+          <label>Date:</label>
+          <input type='date' value={date} onChange={handleDateChange} />
+        </div>
+        <div className='add-item'>
+          <button onClick={addItem}>Add Item</button>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th style={{ borderRadius: '5px 0px 0px 5px' }}>ID</th>
+              <th>Name</th>
+              <th>Date</th>
+              <th style={{ borderRadius: '0px 5px 5px 0px' }}>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, index) => (
+              <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.date}</td>
+                <td>
+                  <button onClick={() => deleteItem(index)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        </div>
+      </div>
+      <div className='col-md-6'>
+        <h2>Q&A Coordinator</h2>
+      </div>
+      {/* tag input */}
+      <div className='col-md-12'>
+      <div className='tagInput'>
+      <TagsInput value={tags} onChange={handleChange} />
     </div>
-  )
-}
+      </div>
+    </div>
+    </div>
+  );
+};
--- src/Pages/QaManagement/QaManagement.jsx
+++ src/Pages/QaManagement/QaManagement.jsx
@@ -1,43 +1,46 @@
 import React, { useState } from 'react';
 import { Col } from 'react-bootstrap';
-import './QaManagement.scss'
+import './QaManagement.scss';
 import { NavLink, Outlet } from 'react-router-dom';
 
 export const QaManagement = () => {
-    
   return (
-    <Col md={{ span: 6 }}>
+    <Col md={{ span: 12 }}>
       <div className='QA'>
-      <div className='title'>
+        <div className='title'>
           <h3>Q&A Management</h3>
         </div>
-        <div className="row">
-          <div className="col-md-2">
-          <div>
+        <div className='row manage '>
+          <div className='col-md-2 list_item'>
             <div>
-            <NavLink to='department'>Department</NavLink>
+              <NavLink to='department'>Department</NavLink>
             </div>
+
             <div>
-            <NavLink to='categories'>Categories</NavLink>
+              <NavLink to='categories'>Categories</NavLink>
             </div>
-           <div>
-           <NavLink to='downloadData'>Download Data</NavLink>
-           </div>
-           <div>
-           <NavLink to='statistical'>Statistical</NavLink>
-           </div>
-           <div>
-           <NavLink to='academic'>Academic Year</NavLink>
-           </div>
+
+            <div>
+              <NavLink to='downloadData'>Download Data</NavLink>
+            </div>
+
+            <div>
+              <NavLink to='statistical'>Statistical</NavLink>
+            </div>
+
+            <div>
+              <NavLink to='academic'>Academic Year</NavLink>
+            </div>
           </div>
+          
+          <div className='items'>
+            <main>
+              <Outlet />
+            </main>
           </div>
         </div>
-          <main>
-            <Outlet />
-          </main>
       </div>
     </Col>
-   
-  )
-}
+  );
+};
 export default QaManagement;
