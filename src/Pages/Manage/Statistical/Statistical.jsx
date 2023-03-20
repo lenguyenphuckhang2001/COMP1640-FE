ffcb3af23d935a73d64644065616a58ec452efe6
Trương Văn Quang <truongvanquang@Quang-Trng.local>
2023-03-20 23:36:02 +0700

    create statistical

--- src/Pages/Manage/Statistical/Statistical.jsx
+++ src/Pages/Manage/Statistical/Statistical.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 export const Statistical = () => {
+  const [mostLikedPost, setMostLikedPost] = useState({});
+  const [mostCommentedPost, setMostCommentedPost] = useState({});
+
+  useEffect(() => {
+    fetch('https://example.com/posts/most-liked')
+      .then(response => response.json())
+      .then(data => setMostLikedPost(data));
+
+    fetch('https://example.com/posts/most-commented')
+      .then(response => response.json())
+      .then(data => setMostCommentedPost(data));
+  }, []);
+
   return (
-    <div>Statistical</div>
+    <div>
+      <h2>Statistics</h2>
+      <p>Most liked post: {mostLikedPost.title}</p>
+      <p>Likes: {mostLikedPost.likes}</p>
+      <p>Comments: {mostLikedPost.comments}</p>
+      <p>Most commented post: {mostCommentedPost.title}</p>
+      <p>Likes: {mostCommentedPost.likes}</p>
+      <p>Comments: {mostCommentedPost.comments}</p>
+    </div>
   )
 }
