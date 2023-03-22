65e91767ed0f03a6c47a465afce940609e9a3e2e
thiengoten <thiengoten6@gmail.com>
2023-03-22 18:19:57 +0700

    refactor axios api

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import PostApi from '../../Api/PostApi';
 import { ListPost } from '../../Components/ListPost/Listpost';
 export const Home = () => {
+  useEffect(() => {
+    (async () => {
+      const response = await PostApi.getAll();
+      console.log('🚀 ~ file: Home.jsx:8 ~ response:', response);
+    })();
+  }, []);
+
   return (
-    <>  
-        <ListPost/>
+    <>
+      <ListPost />
     </>
   );
 };
