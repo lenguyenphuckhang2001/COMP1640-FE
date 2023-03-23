06037f5cdf14844923fa6d5bfb2b95079183f0d2
Trương Văn Quang <truongvanquang@Quang-Trng.local>
2023-03-23 22:44:20 +0700

    style statistiscal and create academic

--- src/Components/Manage/Academic/Academic.jsx
+++ src/Components/Manage/Academic/Academic.jsx
@@ -1,7 +1,112 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+import './Academic.scss';
+
 export const Academic = () => {
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [data, setData] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [editId, setEditId] = useState(null);
+
+  const handleStartDateChange = (event) => {
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event) => {
+    setEndDate(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (editId !== null) {
+      // Edit existing data
+      const newData = [...data];
+      const index = newData.findIndex((item) => item.id === editId);
+      newData[index] = { id: editId, startDate, endDate };
+      setData(newData);
+      setEditId(null);
+    } else {
+      // Create new data
+      const newData = {
+        id: Date.now(),
+        startDate,
+        endDate,
+      };
+      setData([...data, newData]);
+    }
+    setStartDate('');
+    setEndDate('');
+    setShowForm(false);
+  };
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const handleShowForm = () => {
+    setShowForm(!showForm);
+    setEditId(null);
+  };
+
+  const handleEdit = (id) => {
+    const item = data.find((item) => item.id === id);
+    setStartDate(item.startDate);
+    setEndDate(item.endDate);
+    setEditId(id);
+    setShowForm(true);
+  };
+
   return (
-    <div>Academic</div>
-  )
-}
+    <div className='academic'>
+      <div>
+        <h1>Academic Year</h1>
+        {showForm ? (
+          <form onSubmit={handleSubmit}>
+            <div className='date'>
+              <label htmlFor='startDate'>Start Date:</label>
+              <input
+                type='date'
+                id='startDate'
+                value={startDate}
+                onChange={handleStartDateChange}
+              />
+              <label htmlFor='endDate'>End Date:</label>
+              <input type='date' id='endDate' value={endDate} onChange={handleEndDateChange} />
+            </div>
+            <br />
+            <button type='submit'>{editId !== null ? 'Save Changes' : 'Submit'}</button>
+            <button type='button' onClick={handleShowForm}>
+              Cancel
+            </button>
+          </form>
+        ) : (
+          <button onClick={handleShowForm}>Create Academic</button>
+        )}
+        <table>
+          <thead>
+            <tr>
+              <th>Start Date</th>
+              <th>End Date</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.startDate}</td>
+                <td>{item.endDate}</td>
+                <td>
+                  <button onClick={() => handleEdit(item.id)}>Edit</button>
+                </td>
+                <td>
+                  <button onClick={() => handleDelete(item.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
--- src/Components/Manage/Statistical/Statistical.jsx
+++ src/Components/Manage/Statistical/Statistical.jsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from 'react';
+import './Statistical.scss';
 export const Statistical = () => {
   const [mostLikedPost, setMostLikedPost] = useState({});
   const [mostCommentedPost, setMostCommentedPost] = useState({});
 
   useEffect(() => {
     fetch('https://example.com/posts/most-liked')
-      .then(response => response.json())
-      .then(data => setMostLikedPost(data));
+      .then((response) => response.json())
+      .then((data) => setMostLikedPost(data));
 
     fetch('https://example.com/posts/most-commented')
-      .then(response => response.json())
-      .then(data => setMostCommentedPost(data));
+      .then((response) => response.json())
+      .then((data) => setMostCommentedPost(data));
   }, []);
 
   return (
-    <div>
+    <div className='statistical'>
       <h2>Statistics</h2>
       <p>Most liked post: {mostLikedPost.title}</p>
       <p>Likes: {mostLikedPost.likes}</p>
@@ -23,5 +24,5 @@ export const Statistical = () => {
       <p>Likes: {mostCommentedPost.likes}</p>
       <p>Comments: {mostCommentedPost.comments}</p>
     </div>
-  )
-}
+  );
+};
