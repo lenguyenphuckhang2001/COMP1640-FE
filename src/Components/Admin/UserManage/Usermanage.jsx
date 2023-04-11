b8235c1d7227762b1a875ef07c4d2ce8426efefb
huydang <danghuy6584@gmail.com>
2023-04-11 23:04:13 +0700

    final crud

--- src/Components/Admin/UserManage/Usermanage.jsx
+++ src/Components/Admin/UserManage/Usermanage.jsx
@@ -4,14 +4,13 @@ import { Col } from 'react-bootstrap';
 import { useQuery } from 'react-query';
 import UserApi from '../../../Api/UserApi';
 import './Usermanage.scss';
-import { Link, Outlet, useLocation } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useMutation } from 'react-query';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 export const Usermanage = () => {
-  let params = useLocation();
   const [users, setUsers] = useState([]);
   const [inputs, setInputs] = useState({
     email: '',
@@ -19,15 +18,13 @@ export const Usermanage = () => {
     name: '',
     role: 'user',
   });
+  const [refreshKey, setRefreshKey] = useState(0);
   useEffect(() => {
     (async () => {
       const res = await UserApi.getAll();
-      setUsers(res)
-      console.log(res)
+      setUsers(res);
     })();
-  }, []);
-  
-  const [startDate, setStartDate] = useState(new Date());
+  }, [refreshKey]);
   const handleInput = (e) => {
     const nameInput = e.target.name;
     const value = e.target.value;
@@ -76,90 +73,98 @@ export const Usermanage = () => {
       mutation.mutate(data);
     }
   };
-
+  const handleDelete = async (e) => {
+    const todoToDelete = users.find((users) => users._id === e.target.id);
+    const id = todoToDelete._id;
+    try {
+      const res = await UserApi.delete(id);
+      console.log(res)
+      setRefreshKey(oldKey => oldKey +1)
+      const file = await ToastNotice("Dellete success");
+    } catch (res) {
+      const file = await ToastNotice("Dellete error");
+    }
+  }
+  const ToastNotice = (file) => {
+    toast.success(file);
+  }; 
   return (
     <Col md={9}>
       <div className='Usermanage'>
         <h1>User Management</h1>
         <div className='Createuser-page'>
-      <div className='Auth-form-container'>
-        <ToastContainer
-          position='top-right'
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme='light'
-        />
-        <form className='Auth-form' onSubmit={handleSubmit}>
-          <div className='Auth-form-content'>
-            <h3 className='Auth-form-title'>Form create user</h3>
-            <div className='input-group-re'>
-              <input
-                required
-                type='text'
-                name='name'
-                autoComplete='off'
-                className='input'
-                onChange={handleInput}
-              />
-              <label className='user-label'>Name</label>
-            </div>
-            <div className='input-group-re right'>
-              <input
-                required
-                type='email'
-                name='email'
-                autoComplete='off'
-                className='input'
-                onChange={handleInput}
-              />
-              <label className='user-label'>Email</label>
-            </div>
-            <div className='input-group-re'>
-              <input
-                required
-                type='password'
-                name='password'
-                autoComplete='off'
-                className='input'
-                onChange={handleInput}
-              />
-              <label className='user-label'>Password</label>
-            </div>
-            <div className='input-group-re'>
-              <DatePicker
-                selected={startDate}
-                dateFormat='dd/MM/yyyy'
-                maxDate={new Date()}
-                onChange={(date) => setStartDate(date)}
-              />
-            </div>
-            <div className='select-group-re'>
-              <lable className='role-lable'>
-                Role :
-                <select name='role' onChange={handleInput} value={inputs.role}>
-                  <option value={'user'}>User</option>
-                  <option value={'qa'}>QA</option>
-                  <option value={'qa_coordinator'}>QA Coordinator</option>
-                  <option value={'admin'}>Admin</option>
-                </select>
-              </lable>
-            </div>
-            <div className='sm-regis'>
-              <button type='submit' className='btn btn-primary'>
-                {mutation.isLoading ? 'isloading' : 'Create user'}
-              </button>
-            </div>
+          <ToastContainer
+            position='top-right'
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme='light'
+          />
+          <div className='Auth-form-container'>
+            <form className='Auth-form' onSubmit={handleSubmit}>
+              <div className='Auth-form-content'>
+                <h3 className='Auth-form-title'>Form create user</h3>
+                <div className='input-group-re'>
+                  <input
+                    required
+                    type='text'
+                    name='name'
+                    autoComplete='off'
+                    className='input'
+                    onChange={handleInput}
+                    value={inputs.username}
+                  />
+                  <label className='user-label'>Name</label>
+                </div>
+                <div className='input-group-re right'>
+                  <input
+                    required
+                    type='email'
+                    name='email'
+                    autoComplete='off'
+                    className='input'
+                    onChange={handleInput}
+                    value={inputs.email}
+                  />
+                  <label className='user-label'>Email</label>
+                </div>
+                <div className='input-group-re'>
+                  <input
+                    required
+                    type='password'
+                    name='password'
+                    autoComplete='off'
+                    className='input'
+                    onChange={handleInput}
+                    value={inputs.password}
+                  />
+                  <label className='user-label'>Password</label>
+                </div>
+                <div className='select-group-re'>
+                  <lable className='role-lable'>
+                    Role :
+                    <select name='role' onChange={handleInput} value={inputs.role}>
+                      <option value={'user'}>User</option>
+                      <option value={'qa'}>QA</option>
+                      <option value={'qa_coordinator'}>QA Coordinator</option>
+                      <option value={'admin'}>Admin</option>
+                    </select>
+                  </lable>
+                </div>
+                <div className='sm-regis'>
+                <button type='submit'>
+                    {mutation.isLoading ? 'isloading' : inputs._id !== undefined ? 'Update User' : 'Add User'}
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-    </div>
-        <Outlet />
+        </div>
         <table>
           <thead>
             <tr>
@@ -176,8 +181,7 @@ export const Usermanage = () => {
                 <td>{user.username}</td>
                 <td>{user.email}</td>
                 <td>
-                  <button onClick={() => handleEditUser(user)}>Edit</button>
-                  <button onClick={() => handleDeleteUser(user.id)}>Delete</button>
+                  <button onClick={handleDelete} id={user._id}>Delete</button>
                 </td>
               </tr>
             ))}
--- src/Components/Manage/Categories/Categories.jsx
+++ src/Components/Manage/Categories/Categories.jsx
@@ -4,6 +4,7 @@ import './Categories.scss';
 import TagApi from '../../../Api/TagApi';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Col } from 'react-bootstrap';
 
 export const Categories = () => {
   const [todos, setTodos] = useState([]);
@@ -12,166 +13,167 @@ export const Categories = () => {
     name: '',
     description: '',
   });
-
+  const [refreshKey, setRefreshKey] = useState(0);
   useEffect(() => {
     (async () => {
       const res = await TagApi.getAll();
       setTodos(res);
     })();
-  }, [todos]);
-  
+  }, [refreshKey]);
+
   const handleInput = (e) => {
     const nameInput = e.target.name;
     const value = e.target.value;
     setInputs((state) => ({ ...state, [nameInput]: value }));
   };
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     let xx = 1;
-    console.log(inputs)
+    console.log(inputs);
     if (inputs.name == '') {
-        xx = 2;   
-        const file = await ToastNotice("Please enter name") 
+      xx = 2;
+      const file = await ToastNotice('Please enter name');
     }
     if (inputs.description == '') {
-        xx = 2;
-        const file = await ToastNotice("Please enter description") 
+      xx = 2;
+      const file = await ToastNotice('Please enter description');
     }
     if (xx == 1) {
-      const file = await ToastNotice("Success create tag") 
-      if(inputs._id !== undefined){
-        const id = inputs._id
-        const data = {
-        name: inputs.name,
-        description: inputs.description,
-        };
-        const res = await TagApi.update(id, data);
-        console.log(res);
-        console.log(inputs)
-      }
-      else{
+      const file = await ToastNotice('Success create tag');
+      if (inputs._id !== undefined) {
+        const id = inputs._id;
         const data = {
           name: inputs.name,
           description: inputs.description,
-          };
-          const res = await TagApi.create(data);
-          console.log(res);
+        };
+        const res = await TagApi.update(id, data);
+        setRefreshKey(oldKey => oldKey +1)
+        console.log(res);
+        console.log(inputs);
+      } else {
+        const data = {
+          name: inputs.name,
+          description: inputs.description,
+        };
+        const res = await TagApi.create(data);
+        setRefreshKey(oldKey => oldKey +1)
+        console.log(res);
       }
     }
   };
 
   const deleteTodo = async (e) => {
     const todoToDelete = todos.find((todo) => todo._id === e.target.id);
-    if(todoToDelete){
-      const id = todoToDelete._id
+    if (todoToDelete) {
+      const id = todoToDelete._id;
       try {
         const res = await TagApi.delete(id);
-        const file = await ToastNotice(res.message) 
-      } catch (res) { 
-        const file = await ToastNotice(res.response.data.error.message) 
-        // setError(res.response.data.error.message)
+        setRefreshKey(oldKey => oldKey +1)
+        const file = await ToastNotice(res.message);
+      } catch (res) {
+        const file = await ToastNotice(res.response.data.error.message);
       }
     }
-      const file = await ToastNotice()  
-    };
+    const file = await ToastNotice();
+  };
 
   const ToastNotice = (file) => {
     toast.success(file);
   };
-  // const ToastSucces = (file) => {
-  //   toast.success('Success create tag');
-  // };
+
   const handleEdit = (e) => {
     const todoToEdit = todos.find((todo) => todo._id === e.target.id);
-    if(todoToEdit){
-       setInputs(todoToEdit)
-       setShowCreateForm(true);
+    if (todoToEdit) {
+      setInputs(todoToEdit);
+      setShowCreateForm(true);
     }
   };
   const handleReturn = (e) => {
-    if(inputs){
-      setInputs({})
-      setShowCreateForm(false)
+    if (inputs) {
+      setInputs({});
+      setShowCreateForm(false);
     }
   };
   return (
-    <div className='categories'>
-      <div className='crud_categories'>
-      <ToastContainer
-          position='top-right'
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme='dark'
-        />
-        <div>
-          <h1>Categories</h1>
-          <button onClick={() => setShowCreateForm(!showCreateForm)}>Create New Categories</button>
-          {showCreateForm && (
-            <div>
-              <form onSubmit={handleSubmit}>
-                <label htmlFor='name' className='todo-label'>
-                  Name categories:{' '}
-                </label>
-                <input
-                  type='text'
-                  id='name'
-                  name='name'
-                  onChange={handleInput}
-                  className='todo-input'
-                  value={inputs.name}
-                />
-                <br />
-                <label htmlFor='name' className='todo-label'>
-                  Description categories
-                </label>
-                <textarea name='description' onChange={handleInput} value={inputs.description} />
-                <br />
-                <button type='submit'>
-                {inputs._id !== undefined ? ('Update Categories') : ('Add categories')}
-                </button>
-                <button onClick={handleReturn}>Cancel</button>
-              </form>
-            </div>
-          )}
-          <table>
-            <thead>
-              <tr>
-                <th style={{ borderRadius: '5px 0 0 5px' }}>Number</th>
-                <th>Name</th>
-                <th>Date</th>
-                <th style={{ borderRadius: '0 5px 5px 0 ' }}>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {todos.map((todo, key) => (
-                <tr className='column'>
-                  <td>{key + 1}</td>
-                  <td>{todo.name}</td>
-                  <td>{todo.updatedAt.substring(0, 10)}</td>
-                  <td>
-                    <button className='edit' onClick={handleEdit} id={todo._id}>
-                      Edit
-                    </button>
-                    <button
-                      className='del'
-                      id={todo._id}
-                      onClick={deleteTodo}>
-                      Delete
-                    </button>
-                  </td>
+    <Col md={9}>
+      {' '}
+      <div className='categories'>
+        <div className='crud_categories'>
+          <ToastContainer
+            position='top-right'
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme='dark'
+          />
+          <div>
+            <h1>Categories</h1>
+            <button onClick={() => setShowCreateForm(!showCreateForm)}>
+              Create New Categories
+            </button>
+            {showCreateForm && (
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <label htmlFor='name' className='todo-label'>
+                    Name categories:{' '}
+                  </label>
+                  <input
+                    type='text'
+                    id='name'
+                    name='name'
+                    onChange={handleInput}
+                    className='todo-input'
+                    value={inputs.name}
+                  />
+                  <br />
+                  <label htmlFor='name' className='todo-label'>
+                    Description categories
+                  </label>
+                  <textarea name='description' onChange={handleInput} value={inputs.description} />
+                  <br />
+                  <button type='submit'>
+                    {inputs._id !== undefined ? 'Update Categories' : 'Add categories'}
+                  </button>
+                  <button onClick={handleReturn}>Cancel</button>
+                </form>
+              </div>
+            )}
+            <table>
+              <thead>
+                <tr>
+                  <th style={{ borderRadius: '5px 0 0 5px' }}>Number</th>
+                  <th>Name</th>
+                  <th>Date</th>
+                  <th style={{ borderRadius: '0 5px 5px 0 ' }}>Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {todos.map((todo, key) => (
+                  <tr className='column'>
+                    <td>{key + 1}</td>
+                    <td>{todo.name}</td>
+                    <td>{todo.updatedAt.substring(0, 10)}</td>
+                    <td>
+                      <button className='edit' onClick={handleEdit} id={todo._id}>
+                        Edit
+                      </button>
+                      <button className='del' id={todo._id} onClick={deleteTodo}>
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
+    </Col>
   );
 };
