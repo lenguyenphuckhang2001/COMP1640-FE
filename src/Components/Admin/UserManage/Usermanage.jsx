2e1d24c7369cb8bbac5a6b838ae819f1fd3091a6
danghuy6584 <84719301+danghuy6584@users.noreply.github.com>
2023-04-12 14:19:17 +0700

    Feature/huy/rest api cruđ user (#53)
    * crud user
    
    * final crud
    
    * fix create-user

--- src/Components/Admin/UserManage/Usermanage.jsx
+++ src/Components/Admin/UserManage/Usermanage.jsx
@@ -1,50 +1,180 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Col } from 'react-bootstrap';
-import ReactPaginate from 'react-paginate';
 import { useQuery } from 'react-query';
 import UserApi from '../../../Api/UserApi';
-import { FcPrevious, FcNext } from 'react-icons/Fc';
 import './Usermanage.scss';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { useMutation } from 'react-query';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const Usermanage = () => {
-  let params = useLocation();
   const [users, setUsers] = useState([]);
-
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+    name: '',
+    phonenumber:'',
+    role: 'user',
+  });
+  const [refreshKey, setRefreshKey] = useState(0);
   useEffect(() => {
-    axios.get('http://localhost:3000/api/users').then((response) => {
-      setUsers(response.data);
-    });
-  }, []);
-
-  const handleEditUser = (user) => {
-    setName(user.name);
-    setEmail(user.email);
-    setEditing(true);
-    setEditingUser(user);
+    (async () => {
+      const res = await UserApi.getAll();
+      setUsers(res);
+    })();
+  }, [refreshKey]);
+  const handleInput = (e) => {
+    const nameInput = e.target.name;
+    const value = e.target.value;
+    setInputs((state) => ({ ...state, [nameInput]: value }));
   };
-
-  const handleDeleteUser = (id) => {
-    axios.delete(`/api/users/${id}`).then(() => {
-      const updatedUsers = users.filter((user) => user.id !== id);
-      setUsers(updatedUsers);
-    });
+  const mutation = useMutation({
+    mutationFn: async (data) => {
+      return await UserApi.register(data);
+    },
+    onError: async (error) => {
+      const toastError = await ToastError(error.response.data.message);
+    },
+    onSuccess: async () => {
+      const toastSuccess = await ToastSucces();
+    },
+  });
+  const ToastError = (file) => {
+    toast.success(file);
   };
+  const ToastSucces = (file) => {
+    toast.success('Success create user');
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    let xx = 1;
+    if (inputs.email == '') {
+      xx = 2;
+    }
+    if (inputs.password == '') {
+      xx = 2;
+    }
+    if (inputs.name == '') {
+      xx = 2;
+    }
+    if (inputs.phonenumber == '') {
+      xx = 2;
+    }
+    if (xx == 1) {
+      const data = {
+        username: inputs.name,
+        email: inputs.email,
+        password: inputs.password,
+        phonenumber: inputs.phonenumber,
+        role: inputs.role,
+      };
+      mutation.mutate(data);
+    }
+  };
+  const handleDelete = async (e) => {
+    const todoToDelete = users.find((users) => users._id === e.target.id);
+    const id = todoToDelete._id;
+    try {
+      const res = await UserApi.delete(id);
+      console.log(res)
+      setRefreshKey(oldKey => oldKey +1)
+      const file = await ToastNotice("Dellete success");
+    } catch (res) {
+      const file = await ToastNotice("Dellete error");
+    }
+  }
+  const ToastNotice = (file) => {
+    toast.success(file);
+  }; 
   return (
     <Col md={9}>
       <div className='Usermanage'>
         <h1>User Management</h1>
-        <div className='add-user'>
-          <button type='submit'>
-            {params['pathname'].includes('createuser') ? (
-              <Link to='/Account/admin/user/'>Final create user</Link>
-            ) : (
-              <Link to='createuser'>Create User</Link>
-            )}
-          </button>
+        <div className='Createuser-page'>
+          <ToastContainer
+            position='top-right'
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme='light'
+          />
+          <div className='Auth-form-container'>
+            <form className='Auth-form' onSubmit={handleSubmit}>
+              <div className='Auth-form-content'>
+                <h3 className='Auth-form-title'>Form create user</h3>
+                <div className='input-group-re'>
+                  <input
+                    required
+                    type='text'
+                    name='name'
+                    autoComplete='off'
+                    className='input'
+                    onChange={handleInput}
+                  />
+                  <label className='user-label'>Name</label>
+                </div>
+                <div className='input-group-re right'>
+                  <input
+                    required
+                    type='email'
+                    name='email'
+                    autoComplete='off'
+                    className='input'
+                    onChange={handleInput}
+                  />
+                  <label className='user-label'>Email</label>
+                </div>
+                <div className='input-group-re'>
+                  <input
+                    required
+                    type='password'
+                    name='password'
+                    autoComplete='off'
+                    className='input'
+                    onChange={handleInput}
+                  />
+                  <label className='user-label'>Password</label>
+                </div>
+                <div className='input-group-re'>
+                  <input
+                    required
+                    type='text'
+                    name='phonenumber'
+                    autoComplete='off'
+                    className='input'
+                    onChange={handleInput}
+                  />
+                  <label className='user-label'>Phone Number</label>
+                </div>
+                <div className='select-group-re'>
+                  <lable className='role-lable'>
+                    Role :
+                    <select name='role' onChange={handleInput} value={inputs.role}>
+                      <option value={'user'}>User</option>
+                      <option value={'qa'}>QA</option>
+                      <option value={'qa_coordinator'}>QA Coordinator</option>
+                      <option value={'admin'}>Admin</option>
+                    </select>
+                  </lable>
+                </div>
+                <div className='sm-regis'>
+                <button type='submit'>
+                    {mutation.isLoading ? 'isloading' : inputs._id !== undefined ? 'Update User' : 'Add User'}
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-        <Outlet />
         <table>
           <thead>
             <tr>
@@ -56,13 +186,12 @@ export const Usermanage = () => {
           </thead>
           <tbody>
             {users.map((user) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.name}</td>
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.username}</td>
                 <td>{user.email}</td>
                 <td>
-                  <button onClick={() => handleEditUser(user)}>Edit</button>
-                  <button onClick={() => handleDeleteUser(user.id)}>Delete</button>
+                  <button onClick={handleDelete} id={user._id}>Delete</button>
                 </td>
               </tr>
             ))}
@@ -72,54 +201,3 @@ export const Usermanage = () => {
     </Col>
   );
 };
-// export const Usermanage = () => {
-//     const [itemOffset, setItemOffset] = useState(0);
-//     const [itemLimit, setItemLimit] = useState(5);
-
-//     const getAllUsers = async () => {
-//       const res = await UserApi.getAll();
-//       return res;
-//     };
-//     const getAllUser = useQuery({
-//       queryKey: ['users'],
-//       queryFn: getAllUsers,
-//       retry: 7,
-//     });
-//     const currentItems = getAllUser?.data?.slice(itemOffset, itemOffset + itemLimit);
-//     const pageCout = Math.ceil(getAllUser?.data?.length / itemLimit);
-
-//     const handlePageClick = (e) => {
-//       const selectedPage = e.selected;
-//       setItemOffset(selectedPage * itemLimit);
-//     };
-
-//     return (
-//       <Col md={8}>
-//         {getAllUser.isLoading ? (
-//           <h1 className='load-screen'>Loading...</h1>
-//         ) : (
-//           currentItems.map((user) => {
-//             return (
-//               <div className='user'>
-//                 <img src={user.avatar} alt='' />
-//                 <h1 className='load-screen'>{user.name}</h1>
-//                 <h1 className='load-screen'>{user.email}</h1>
-//                 <h1 className='load-screen'>{user.role}</h1>
-//               </div>
-//             );
-//           })
-//         )}
-//         <ReactPaginate
-//           breakLabel='...'
-//           nextLabel={<FcNext />}
-//           pageRangeDisplayed={4}
-//           onPageChange={handlePageClick}
-//           pageCount={pageCout}
-//           previousLabel={<FcPrevious />}
-//           renderOnZeroPageCount={null}
-//           breakLinkClassName='test'
-//           className='test'
-//         />
-//       </Col>
-//     );
-//   };
