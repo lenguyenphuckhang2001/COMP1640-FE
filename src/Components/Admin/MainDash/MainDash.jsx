4ecf1f1e0772220c8326ee1a702e6e47fc5b9848
thiengoten <72801462+thiengoten@users.noreply.github.com>
2023-04-19 01:16:17 +0700

    fix: 🐛 admin dashboard (#60)

--- src/Components/Admin/MainDash/MainDash.jsx
+++ src/Components/Admin/MainDash/MainDash.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 import Cards from '../Cards/Cards';
-import Table from "../Table/Table";
-import "./MainDash.scss"
+import Table from '../Table/Table';
+import './MainDash.scss';
 export const MainDash = () => {
   return (
     <Col md={9}>
-        <div className='MainDash'>
-          <Cards/>
-          <Table/>
-        </div>
+      <div className='MainDash'>
+        <Cards />
+      </div>
     </Col>
   );
 };
